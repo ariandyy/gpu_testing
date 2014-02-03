55fbab07d4dff5109c2980db1f7b8f8e52a02861
ariandyy <ariandyy@gmail.com>
2014-02-03 20:52:03 +0100

    cuda success

--- src/main.cu
+++ src/main.cu
@@ -6,8 +6,16 @@
 using namespace std;
 using namespace cv;
 
-__global__ void parCalcCornerBlockHist(){
-	
+__global__ void cudaCalcCornerBlockHist(unsigned char * src, 
+										int rows, 
+										int cols, 
+										int blockSizeX, 
+										int blockSizeY, 
+										int beginX, 
+										int beginY, 
+										unsigned int * outHist){
+	int idx = rows * (threadIdx.y + beginY) + (threadIdx.x + beginX);
+	atomicAdd(&(outHist[src[idx]]), 1);
 }
 
 
@@ -27,6 +35,40 @@ void calcCornerBlockHist(const Mat src, int blockSizeX, int blockSizeY, int begi
 	cout<<"count = "<<count<<endl;
 }
 
+void processHistogram(unsigned char * hist, int max){
+	float mean = 0;
+	float sum = 0;
+	int occurences = 0;
+	
+	for (int i = 0; i < max; i++){
+		cout<<(int)hist[i]<<" ";
+		sum += (float)i * hist[i];
+		occurences += hist[i];
+	}
+	mean = sum/occurences;
+	cout<<endl;
+	cout<<"sum = "<<sum<<endl;
+	cout<<"occurences = "<<occurences<<endl;
+	cout<<"mean = "<<mean<<endl;
+}
+
+void processHistogram(unsigned int * hist, int max){
+	float mean = 0;
+	float sum = 0;
+	int occurences = 0;
+	
+	for (int i = 0; i < max; i++){
+		cout<<(int)hist[i]<<" ";
+		sum += (float)i * hist[i];
+		occurences += hist[i];
+	}
+	mean = sum/occurences;
+	cout<<endl;
+	cout<<"sum = "<<sum<<endl;
+	cout<<"occurences = "<<occurences<<endl;
+	cout<<"mean = "<<mean<<endl;
+}
+
 int main(int argc, char** argv){
 	if (argc != 2){
 		cout<<"usage:\n";
@@ -38,6 +80,12 @@ int main(int argc, char** argv){
 	
 	Mat matSource;
 	matSource = imread(argv[1], 0);
+	
+	int blockSizeX = 32;
+	int blockSizeY = 32;
+	int startX = 128;
+	int startY = 0;
+	
 	
 	unsigned char hist[256] = {0};
 	
@@ -52,7 +100,7 @@ int main(int argc, char** argv){
 	cudaEventRecord(start, 0);
 	
 	// function call
-	calcCornerBlockHist(matSource, 32, 32, 0, 0, hist);
+	calcCornerBlockHist(matSource, blockSizeX, blockSizeY, startX, startY, hist);
 	// for 128x128 block size, why total occurences is less than 128x128=16384?
 	
 	// timer
@@ -60,29 +108,38 @@ int main(int argc, char** argv){
 	cudaEventSynchronize(stop);
 	float elapsedTime;
 	cudaEventElapsedTime(&elapsedTime, start, stop);
-	cout<<"CPU histogram took "<<setprecision(5)<<elapsedTime<<" ms"<<endl;
+	cout<<"CPU histogram took "<<setprecision(5)<<elapsedTime*1000<<" ns"<<endl;
 	
 	cout<<"histogram"<<endl;
-	float mean = 0;
-	float sum = 0;
-	int occurences = 0;
-	for (int i = 0; i < 256; i++){
-		cout<<(int)hist[i]<<"+";
-		sum += (float)i * hist[i];
-		occurences += hist[i];
-	}
-	mean = sum/occurences;
+	processHistogram(hist, 256);
 	cout<<endl;
-	cout<<"sum = "<<sum<<endl;
-	cout<<"occurences = "<<occurences<<endl;
-	cout<<"mean = "<<mean<<endl;
-	//cout<<"mean = "<<mean/256.0;
-	
-	cout<<endl;
-	//waitKey(0);
 	
 	// cuda
+	unsigned char* host_image = matSource.data;
+	unsigned int host_hist[256];
 	
+	unsigned char* dev_image;
+	size_t size = matSource.rows*matSource.cols*sizeof(unsigned char);
+	cudaMalloc(&dev_image, size);
+	cudaMemcpy(dev_image, host_image, size, cudaMemcpyHostToDevice);
 	
+	unsigned int * dev_hist;
+	cudaMalloc(&dev_hist, 256*sizeof(unsigned int));
+	cudaMemset(dev_hist, 0, 256*sizeof(unsigned int));
+	
+	cudaEventRecord(start, 0);
+	cudaCalcCornerBlockHist<<<1, dim3(32,32, 1)>>>(dev_image, matSource.rows, matSource.cols, blockSizeX, blockSizeY, startX, startY, dev_hist);
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&elapsedTime, start, stop);
+	cout<<"GPU histogram took "<<setprecision(5)<<elapsedTime*1000<<" ns"<<endl;
+	
+	cudaMemcpy(host_hist, dev_hist, 256*sizeof(unsigned int), cudaMemcpyDeviceToHost);
+	cout<<"histogram from cuda"<<endl;
+	processHistogram(host_hist, 256);
+	
+	// cleanup
+	cudaFree(dev_image);
+	cudaFree(dev_hist);
 	return 0;
 }
