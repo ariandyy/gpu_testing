693976e3a449790b1193b0fd7990745ee9900909
ariandyy <ariandyy@gmail.com>
2014-02-03 15:19:09 +0100

    first comit

---
+++ src/main.cu
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cuda.h>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv){
+	if (argc != 2){
+		cout<<"usage:\n";
+		cout<<"  "<<argv[0]<<" [image file]\n";
+		return -1;
+	}
+	
+	Mat matSource;
+	matSource = imread(argv[1], 0);
+	
+	namedWindow("Original", CV_WINDOW_AUTOSIZE);
+	imshow("Original", matSource);
+	
+	
+	waitKey(0);
+	return 0;
+}
